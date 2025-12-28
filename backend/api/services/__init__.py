4bbb4ede24781f24e618b7b4033c5f94c4fac2bd
Bilgisu Cempel <bsucempel@gmail.com>
2025-12-29 01:27:17 +0300

    added bulk student import service and views

--- backend/api/services/__init__.py
+++ backend/api/services/__init__.py
@@ -2,6 +2,11 @@
 # Contains business logic and utility services
 
 from .email_service import EmailService
+from .student_import_service import StudentImportService, StudentImportServiceError
 
-__all__ = ['EmailService']
+__all__ = [
+    'EmailService',
+    'StudentImportService',
+    'StudentImportServiceError',
+]
 
